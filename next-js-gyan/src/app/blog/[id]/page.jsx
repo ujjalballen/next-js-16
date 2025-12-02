d3a89710a3f36a56d901770fa716941dd190c18d
Ujjal B. Allen <ujjal.vision02@gmail.com>
2025-12-02 16:13:05 +0600

    add dynamic route for post details

---
+++ next-js-gyan/src/app/blog/[id]/page.jsx
@@ -0,0 +1,8 @@
+export default function BlogDetailsPage(){
+
+    return(
+        <div>
+            this is blog details page
+        </div>
+    )
+}
--- next-js-gyan/src/app/blog/page.jsx
+++ next-js-gyan/src/app/blog/page.jsx
@@ -1,9 +1,37 @@
-export default function BlogPage(){
+import Link from "next/link";
 
-    return(
+export default function BlogPage() {
+  const posts = [
+    {
+      id: 1,
+      heading: "Post one Heading",
+      paragraph: "This is Post one Paragraph",
+    },
+    {
+      id: 2,
+      heading: "Post two Heading",
+      paragraph: "This is Post two Paragraph",
+    },
+    {
+      id: 3,
+      heading: "Post three Heading",
+      paragraph: "This is Post three Paragraph",
+    },
+  ];
 
-        <div>
-            this is blog page
-        </div>
-    )
+  return (
+    <div className="p-2">
+      <h1>Blog Posts</h1>
+      <div className="grid grid-cols-5 gap-2 mt-3">
+        {posts.map((post) => {
+          return (
+            <div key={post.id} className="border p-4">
+              <Link href={`/blog/${post.id}`} className="text-green-500"><h2>{post.heading}</h2></Link>
+              <p>{post.paragraph}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
